c8d61525123904ed351c447acf7a90fdbaff7b76
ZephirSD <siegfrieddallery@gmail.com>
2024-05-20 11:53:06 +0400

    Création du composant `Footer`

---
+++ components/FooterPage.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <footer class="footer footer-center p-4 bg-base-300 text-base-content">
+      <aside>
+        <p>Copyright © 2024 - All right reserved by VideoGame</p>
+      </aside>
+    </footer>
+  </div>
+</template>
+
+<script setup></script>
